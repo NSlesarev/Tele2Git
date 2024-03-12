ff0b686f003c029b0eaea07272a7db54a5327360
Nik <64489217+NSFerz@users.noreply.github.com>
2024-03-12 14:20:08 +0300

    починил маску для пк

--- src/js/main.js
+++ src/js/main.js
@@ -62,11 +62,10 @@ tableCityMobile.classList.remove("visible");
 headerCityMobile.innerHTML = event.target.innerHTML;
 };
 
-
 // для пк
 document.addEventListener("DOMContentLoaded", function() {
     // Получаем элемент поля ввода
-    let phoneInput = document.getElementById('phone');
+    let phoneInput = document.getElementById('phoneD');
   
     // Устанавливаем маску для ввода
     phoneInput.addEventListener('input', function(e) {
@@ -88,6 +87,26 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener("DOMContentLoaded", function() {
     // Получаем элемент поля ввода
     let phoneInput = document.getElementById('phonem');
+  
+    // Устанавливаем маску для ввода
+    phoneInput.addEventListener('input', function(e) {
+      // Форматируем ввод номера телефона
+      let x = e.target.value.replace(/\D/g, '').match(/(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
+      e.target.value = !x[2] ? x[1] : '+7 (' + x[2] + (x[3] ? ') ' + x[3] : '') + (x[4] ? '-' + x[4] : '') + (x[5] ? '-' + x[5] : '');
+    });
+
+    // При клике на поле ввода номера, включаем код города
+    phoneInput.addEventListener('focus', function(e) {
+      if (!e.target.value.startsWith('+7')) {
+        e.target.value = '+7';
+      }
+    });
+});
+
+// для модул
+document.addEventListener("DOMContentLoaded", function() {
+    // Получаем элемент поля ввода
+    let phoneInput = document.getElementById('phone');
   
     // Устанавливаем маску для ввода
     phoneInput.addEventListener('input', function(e) {
